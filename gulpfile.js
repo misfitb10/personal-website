672a49901d413cda1b944178065d65ab595aa70a
Bilal Hussain <bilal.hussain@incentro.com>
2018-05-19 17:15:36 +0200

    Implemented gulpconfig JSON file for paths, updated packages, more functions for tasks, added sounds for copy to build

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,87 @@
-var gulp = require('gulp');
-var babel = require('gulp-babel');
-var htmlmin = require('gulp-htmlmin');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var cleanCSS = require('gulp-clean-css');
-var del = require('del');
+"use strict";
 
-var paths = {
-    html: {
-        src: 'src/*.html',
-        dest: 'build/'
-    },
-    styles: {
-        src: 'src/scss/**/*.scss',
-        dest: 'build/css/'
-    },
-    scripts: {
-        src: 'src/js/*.js',
-        dest: 'build/js/'
-    }
-};
+// Config file for Gulp
+const config = require('./gulpconfig.json');
+
+const gulp = require('gulp'),
+    gulpLoadPlugins = require('gulp-load-plugins'),
+    plugins = gulpLoadPlugins(),
+    sass = require('gulp-sass'),
+    del = require('del'),
+    yargs = require('yargs').argv,
+    minify = !!yargs.minify;
 
 function clean() {
-    return del([ 'assets' ]);
+    return del(config.path.build.root);
 }
 
 function html() {
-    return gulp.src(paths.html.src)
-        .pipe(htmlmin({
-            removeComments: true,
-            collapseWhitespace: true
-        }))
-        .pipe(gulp.dest(paths.html.dest));
+    return gulp.src(config.path.src.html)
+        .pipe(plugins.htmlmin({collapseWhitespace: true, removeComments: true}))
+        .pipe(gulp.dest(config.path.build.root))
+        .pipe(plugins.size({title: '--> HTML'}));
+}
+
+function images() {
+    return gulp.src(config.path.src.images)
+        .pipe(plugins.imagemin([
+            plugins.imagemin.gifsicle({interlaced: true}),
+            plugins.imagemin.jpegtran({progressive: true}),
+            plugins.imagemin.optipng({optimizationLevel: 5})]))
+        .pipe(gulp.dest(config.path.build.images))
+        .pipe(plugins.size({title: '--> Images'}));
+}
+
+function sounds() {
+    return gulp.src(config.path.src.sounds)
+        .pipe(gulp.dest(config.path.build.sounds))
+        .pipe(plugins.size({title: '--> Sounds'}));
 }
 
 function styles() {
-    return gulp.src(paths.styles.src)
-        .pipe(sass())
-        .pipe(cleanCSS())
-        .pipe(rename({
+    return gulp.src(config.path.src.styles)
+        .pipe(plugins.sass().on('error', sass.logError))
+        .pipe(plugins.autoprefixer(config.path.autoprefixer.split(', ')))
+        .pipe(plugins.if(minify, plugins.cssnano()))
+        .pipe(plugins.rename({
             basename: 'main',
             suffix: '.min'
         }))
-        .pipe(gulp.dest(paths.styles.dest));
+        .pipe(gulp.dest(config.path.build.styles))
+        .pipe(plugins.size({title: '--> CSS'}));
 }
 
 function scripts() {
-    return gulp.src(paths.scripts.src, { sourcemaps: true })
-        .pipe(babel())
-        .pipe(uglify())
-        .pipe(concat('main.min.js'))
-        .pipe(gulp.dest(paths.scripts.dest));
+    return gulp.src(config.path.src.scripts, {sourcemaps: true})
+        .pipe(plugins.if(minify, plugins.uglify()))
+        .pipe(plugins.concat('main.min.js'))
+        .pipe(gulp.dest(config.path.build.scripts))
+        .pipe(plugins.size({title: '--> Scripts'}));
 }
 
 function watch() {
-    gulp.watch(paths.html.src, html);
-    gulp.watch(paths.scripts.src, scripts);
-    gulp.watch(paths.styles.src, styles);
+    gulp.watch(config.path.src.html, html);
+    gulp.watch(config.path.src.images, images);
+    gulp.watch(config.path.src.scripts, scripts);
+    gulp.watch(config.path.src.styles, styles);
 }
 
-var build = gulp.series(gulp.parallel(html, styles, scripts));
-var watch = gulp.series(gulp.parallel(html, styles, scripts), watch);
+exports.clean = clean;
+exports.html = html;
+exports.images = images;
+exports.sounds = sounds;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watch = watch;
 
-gulp.task('build', build);
-gulp.task('default', watch);
-gulp.task('watch', watch);
+const __c = gulp.series(clean);
+const __cb = gulp.series(clean, gulp.parallel(html, images, sounds, styles, scripts));
+const __b = gulp.series(gulp.parallel(html, images, sounds, styles, scripts));
+const __bw = gulp.series(gulp.parallel(html, images, sounds, styles, scripts), watch);
+const __cbw = gulp.series(clean, gulp.parallel(html, images, sounds, styles, scripts), watch);
+
+gulp.task('c', __c);
+gulp.task('cb', __cb);
+gulp.task('cbw', __cbw);
+gulp.task('b', __b);
+gulp.task('bw', __bw);
